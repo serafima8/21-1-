59f60bae338f431b2390aad41455363b4bb1bf65
serafima8 <147659413+serafima8@users.noreply.github.com>
2024-05-07 13:43:23 +0300

    Add files via upload

---
+++ Делегаты (задание 1)/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Делегаты__задание_1_
+{
+    class Program
+    {
+        delegate void GetMessage(); //объявление делегата
+        static void Main(string[] args)
+        {
+            GetMessage del; //создание делегата
+            if (DateTime.Now.Hour < 12)
+            {
+                del = GoodMorning; //присваивание этой переменной адрес метода
+            }
+            else
+            {
+                del = GoodEvening; //присваивание этой переменной адрес метода
+            }
+            del.Invoke(); //вызываем метод
+
+            Console.ReadKey();
+        }
+        private static void GoodMorning()
+        {
+            Console.WriteLine("Доброе утро");
+        }
+        private static void GoodEvening()
+        {
+            Console.WriteLine("Добрый вечер");
+        }
+    }
+}
